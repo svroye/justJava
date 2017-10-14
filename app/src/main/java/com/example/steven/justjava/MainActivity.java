3efd73a7856806a03fc9feb043c0c57714fbdb28
Stevenvr <vanroyesteven@gmail.com>
2017-10-14 22:57:56 +0200

    Add logic for adding topping price to total price Add logic for negative numbers Add intent for opening email app

--- app/src/main/java/com/example/steven/justjava/MainActivity.java
+++ app/src/main/java/com/example/steven/justjava/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.steven.justjava;
 
 import java.text.NumberFormat;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,19 +29,27 @@ public class MainActivity extends AppCompatActivity {
     public void submitOrder(View view){
         // get the checkbox for whipped cream and its value
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream);
-        boolean whippedCream = whippedCreamCheckBox.isChecked();
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
 
         // get the checkbox for chocolat and its value
         CheckBox chocolatCheckBox = (CheckBox) findViewById(R.id.chocolat);
-        boolean chocolat = chocolatCheckBox.isChecked();
+        boolean hasChocolate = chocolatCheckBox.isChecked();
 
         // get the text from the name field and its value
         EditText nameEditText = (EditText) findViewById(R.id.edit_text_name);
         String name = nameEditText.getText().toString();
 
-        int price = calculatePrice();
-        String summary = createOrderSummary(price, whippedCream, chocolat, name);
-        displayMessage(summary);
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        String summary = createOrderSummary(price, hasWhippedCream, hasChocolate, name);
+        //displayMessage(summary);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, summary);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     /**
@@ -50,22 +62,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    /**
-     *
-     * @param text to be displayed
-     */
-    private void displayMessage(String text){
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(text);
-    }
+//    /**
+//     *
+//     * @param text to be displayed
+//     */
+//    private void displayMessage(String text){
+//        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+//        orderSummaryTextView.setText(text);
+//    }
 
     /**
      *
      * @return total price of the order
      */
-    private int calculatePrice(){
-        int price = quantity * 5;
-        return price;
+    private int calculatePrice(boolean whippedCream, boolean chocolate){
+        int pricePerCoffee = 5;
+
+        if (whippedCream){
+            pricePerCoffee += 1;
+        }
+
+        if (chocolate){
+            pricePerCoffee += 2;
+        }
+
+        return quantity * pricePerCoffee;
     }
 
     /**
@@ -74,13 +95,12 @@ public class MainActivity extends AppCompatActivity {
      * @return summary of the order, including name, quantity, price
      */
     private String createOrderSummary(int price, boolean whippedCream, boolean chocolat, String name){
-        String summary = "Name: " + name +
+        return "Name: " + name +
                 "\nAdd whipped cream? " + whippedCream +
                 "\nAdd chocolat? " + chocolat +
                 "\nQuantity: " + quantity +
                 "\nTotal: " +  NumberFormat.getCurrencyInstance().format(price) +
                 "\nThank you!";
-        return summary;
     }
 
     /**
@@ -97,7 +117,12 @@ public class MainActivity extends AppCompatActivity {
      * @param v
      */
     public void decrement(View v){
-        quantity--;
+        if (quantity > 0) {
+            quantity--;
+        } else {
+            // show error message
+            Toast.makeText(this,"You cannot order less than 1 coffee!",Toast.LENGTH_SHORT).show();
+        }
         displayQuantity(quantity);
     }
 }
